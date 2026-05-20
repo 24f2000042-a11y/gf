c90d4bd5320e475f8c50c5e1db355831e37f60da
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-21 01:03:19 +0530

    add backend/controllers/authController.js

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ username, password: hashedPassword });
+  await user.save();
+  res.status(201).json({ message: 'User created' });
+};
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+};
+
+module.exports = { register, login };
